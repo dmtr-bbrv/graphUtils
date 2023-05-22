7dcf1f3a314975d63244046d08aae9194107a6f3
Dmitry Bobrov <dim.bobrov2017@yandex.ru>
2023-05-22 23:42:22 +0300

    Add node view for graph

---
+++ graphUtils/graphUtils/UIComponents/Node.swift
@@ -0,0 +1,29 @@
+//
+//  Node.swift
+//  graphUtils
+//
+//  Created by MacBook Pro on 22.05.2023.
+//
+
+import SwiftUI
+
+struct Node: View {
+    let position: CGPoint
+    let radius: CGFloat
+    let color: Color
+    let value: Int
+
+    var body: some View {
+        ZStack {
+
+            Circle()
+                .frame(width: radius, height: radius)
+                .foregroundColor(color)
+                .position(position)
+            Text("\(value)")
+                .foregroundColor(.black)
+                .position(position)
+
+        }
+    }
+}
